cfb27ffd12872e49129415c2028a2c450e33b1c5
Carter Bush <carter.l.bush@gmail.com>
2023-10-14 15:35:27 +1100

    Enable cors on server if running in dev

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.IS_DEV) {
+    app.enableCors();
+  }
+
   await app.listen(3000);
 }
 bootstrap();
