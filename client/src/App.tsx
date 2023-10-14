4d0f75969d753ebebf27fae0ab0babd2caa88bd3
Carter Bush <carter.l.bush@gmail.com>
2023-10-14 23:27:46 +1100

    Restyle App component, render built components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+import PlayerOverviewCard from './components/PlayerOverviewCard';
+import PlayerSearch from './components/PlayerSearch';
 import './App.css';
+import { Paper } from '@mui/material';
 
 function App() {
-  return <div></div>;
+  const [playerId, setPlayerId] = useState<string>('');
+
+  return (
+    <Paper sx={{ padding: 4 }}>
+      <PlayerSearch handleIdRetrieved={setPlayerId}></PlayerSearch>
+      {playerId && <PlayerOverviewCard playerId={playerId} />}
+    </Paper>
+  );
 }
 
 export default App;
