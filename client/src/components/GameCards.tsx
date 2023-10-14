dc2e27056ebc00c45029a114691a02fe2dbb1c9b
Carter Bush <carter.l.bush@gmail.com>
2023-10-15 01:25:31 +1100

    Add ability to sort games based on time played

--- client/src/components/GameCards.tsx
+++ client/src/components/GameCards.tsx
@@ -1,5 +1,12 @@
 import { HourglassBottom } from '@mui/icons-material';
-import { Box, Card, Grid, TextField } from '@mui/material';
+import {
+  Box,
+  Card,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  TextField,
+} from '@mui/material';
 import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
 import { Game } from '../models/game';
 import api from '../api';
@@ -13,18 +20,33 @@ const GameCards: React.FC<GameCardsProps> = ({ playerId }) => {
   const [playerGames, setPlayerGames] = useState<Game[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [gameNameFilter, setGameNameFilter] = useState<string>('');
+  const [orderByTimePlayed, setOrderByTimePlayed] = useState<boolean>(false);
+
   const onGameNameFilterChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => setGameNameFilter(e.target.value),
     [setGameNameFilter],
   );
 
-  const filteredGames = useMemo(() => {
-    return playerGames.filter(
+  const onOrderByTimePlayedChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setOrderByTimePlayed(e.target.checked);
+    },
+    [setOrderByTimePlayed],
+  );
+
+  const filteredAndSortedGames = useMemo(() => {
+    const filteredGames = playerGames.filter(
       (g) =>
         !gameNameFilter ||
         g.name.toLowerCase().match(gameNameFilter.toLowerCase()),
     );
-  }, [playerGames, gameNameFilter]);
+
+    const sortedGames = filteredGames.sort((a, b) =>
+      orderByTimePlayed ? b.playTime - a.playTime : 0,
+    );
+
+    return sortedGames;
+  }, [playerGames, gameNameFilter, orderByTimePlayed]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -49,16 +71,34 @@ const GameCards: React.FC<GameCardsProps> = ({ playerId }) => {
     </Card>
   ) : (
     <Box>
-      <Box sx={{ mt: 1 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          mt: 1,
+        }}
+      >
         <TextField
           label="Filter games by name"
           placeholder="Counter-Strike"
           value={gameNameFilter}
           onChange={onGameNameFilterChange}
         />
+        <Box sx={{ ml: 2 }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={orderByTimePlayed}
+                onChange={onOrderByTimePlayedChange}
+              />
+            }
+            label="Order by time played"
+          />
+        </Box>
       </Box>
       <Grid container sx={{ mt: 1 }} spacing={1}>
-        {filteredGames.map((game) => (
+        {filteredAndSortedGames.map((game) => (
           <Grid item xs={12} lg={6} xl={4} key={game.appID}>
             <GameCard game={game} />
           </Grid>
